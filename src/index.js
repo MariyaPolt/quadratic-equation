d5a65bb5f633ea608a9a8f9251c3f983dcfaf6d9
Mariya <mariya.poltoratskaya@gmail.com>
2018-09-19 23:37:32 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var substringArray = equation.split(" ");
+  var a = +substringArray[0];
+  var b = +(substringArray[3] + substringArray[4]);
+  var c = +(substringArray[7] + substringArray[8]);
+
+  resultArray = [];
+  resultArray.push( Math.round( (-b + Math.sqrt(b*b - 4*a*c)) / (2 * a) ) );
+  resultArray.push( Math.round( (-b - Math.sqrt(b*b - 4*a*c)) / (2 * a) ) );
+  resultArray.sort( function(a, b) { return a - b; });
+
+  return resultArray;
 }
