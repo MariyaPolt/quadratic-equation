12fd89f342bfa0318912220b99b8d7c01395cc5e
Mariya <37655379+MariyaPolt@users.noreply.github.com>
2018-09-29 16:36:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = function solveEquation(equation) {
-  var substringArray = equation.split(" ");
-  var a = +substringArray[0];
-  var b = +(substringArray[3] + substringArray[4]);
-  var c = +(substringArray[7] + substringArray[8]);
+  let substringArray = equation.split(" ");
+  let a = +substringArray[0];
+  let b = +(substringArray[3] + substringArray[4]);
+  let c = +(substringArray[7] + substringArray[8]);
 
   resultArray = [];
   resultArray.push( Math.round( (-b + Math.sqrt(b*b - 4*a*c)) / (2 * a) ) );
   resultArray.push( Math.round( (-b - Math.sqrt(b*b - 4*a*c)) / (2 * a) ) );
-  resultArray.sort( function(a, b) { return a - b; });
+  resultArray.sort( (a, b) => a - b; );
 
   return resultArray;
 }
